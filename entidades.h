606b109fd5efc1b487fb1a4bce9af353af219524
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-27 08:27:19 -0300

    editando

--- entidades.h
+++ entidades.h
@@ -10,7 +10,7 @@ struct base_t();
 
 struct missao_t();
 
-struct world();
+struct mundo_t();
 
 /* aqui, vou colocar as structs que criam e destroem cada uma das entidades */
 struct heroi_t *cria_heroi(int id_h);
@@ -24,5 +24,9 @@ struct base_t *destroi_base(struct base_t *b);
 struct missao_t *cria_missao(int id_m);
 
 struct missao_t *destroi_missao(struct missao_t *m);
+
+struct missao_t *cria_mundo();
+
+struct missao_t *destroi_mundo(struct mundo_t *w);
 
 #endif
