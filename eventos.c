136102412d337c893a9e85e47ecd7e18485762a4
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-27 14:18:44 -0300

    eventos.c

--- eventos.c
+++ eventos.c
@@ -3,54 +3,66 @@
 #include "eventos.h"
 #include "theboys.h"
 #include "fprio.h"
+#include "fila.h"
 
-/* eventos que causarao as mudancas de estado na simulacao discreta */
-
-/* representa um evento do tipo um, que tem tempo, heroi e base */
-struct ev_t_um{
+/* representa um evento com todas as informacoes possiveis, as quais */
+/* podem ser utilizadas ou nao. */
+/* causara mudancas de estado na simulacao discreta. */
+struct evento_t{
     int tempo;
-    int id_heroi;
-    int id_base;
+    int tipo;
+    int info1;
+    int info2;
 };
 
-/* representa um evento que tem apenas tempo e base, como o caso do avisa */
-struct ev_t_dois{
-    int tempo;
-    int id_base;
-};
-
-/* representa um evento que usa apenas tempo e missao */
-struct ev_t_tres{
-    int tempo;
-    int id_missao;
+/* cria um novo evento, retornando um ponteiro para ele ou NULL em caso de erro */
+struct *cria_evento(struct mundo_t *m, int tempo, int tipo, int info1, int info2){
+    struct evento_t *ev = malloc(sizeof(struct evento_t));
+    if (!ev)
+        return NULL;
+    ev->tempo = tempo;
+    ev->tipo = tipo;
+    ev->info1 = info1;
+    ev->info2 = info2;
+    return evento;
 }
 
 /* representa um heroi H chegando em uma base B no instante T, para depois esperar para entrar na fila ou desistir */
-void chega(struct mundo_t *m, struct ev_t_um *ev);
+void chega(struct mundo_t *m, struct evento_t *ev){
+    struct heroi_t *h = m->vet_h[ev->info1];
+    struct base_t *b = m->vet_b[ev->info2];
+    h->id_b = b->id_b; /* atualiza a base do heroi */
+    int tempo = ev->tempo;
+    int espera;
+    if (cjto_card(b->presentes) < (b->limite) && fila_tamanho(b->espera) == 0)
+        espera = 1; /* valor logico verdadeiro */
+    else espera = (h->paciencia) > (10*fila_tamanho(b->espera));
+
+}
 
 /* representa a entrada do heroi na fila de espera */
-void espera(struct mundo_t *m, struct ev_t_um *ev);
+void espera(struct mundo_t *m, struct evento_t *ev);
 
 /* representa a desistencia de um heroi em esperar para entrar em uma base, e em seguida esse heroi viaja para uma base aleatoria D */
-void desiste(struct mundo_t *m, struct ev_t_um *ev);
+void desiste(struct mundo_t *m, struct evento_t *ev);
 
 /* representa que o porteiro da dada base eh avisado e verifica a fila de espera */
-void avisa(struct mundo_t *m, struct ev_t_dois *ev);
+void avisa(struct mundo_t *m, struct evento_t *ev);
 
 /* representa a entrada do heroi na determinada base, agendando o tempo que vai permanecer la e quando vai sair */
-void entra(struct mundo_t *m, struct ev_t_um *ev);
+void entra(struct mundo_t *m, struct evento_t *ev);
 
 /* representa a saida do heroi da base em que estava, escolhendo uma outra base aonde viajar e avisando o porteiro de que ha uma nova vaga disponivel */
-void sai(struct mundo_t *m, struct ev_t_um *ev);
+void sai(struct mundo_t *m, struct evento_t *ev);
 
 /* representa a viagem do heroi para uma base, agendando e avisando o ponteiro */
-void viaja(struct mundo_t *m, struct ev_t_um *ev);
+void viaja(struct mundo_t *m, struct evento_t *ev);
 
 /* representa a morte de um heroi, liberando uma nova vaga e avisando o porteiro */
-void morre(struct mundo_t *m, struct ev_t_um *ev);
+void morre(struct mundo_t *m, struct evento_t *ev);
 
 /* representa o disparo de uma missao nova no instante T*/
-void evento_missao(struct mundo_t *m, struct ev_t_tres *ev);
+void evento_missao(struct mundo_t *m, struct evento_t *ev);
 
 /* representa o fim da simulacao */
 void fim(struct mundo_t *m);
