e180e984f10aa0873f98b9b1408ae0334b79f2da
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-27 00:59:35 -0300

    inicial eventos.c

---
+++ eventos.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "eventos.h"
+#include "theboys.h"
+#include "fprio.h"
+
+/* eventos que causarao as mudancas de estado na simulacao discreta */
+
+/* representa um evento do tipo um, que tem tempo, heroi e base */
+struct ev_t_um{
+    int tempo;
+    int id_heroi;
+    int id_base;
+};
+
+/* representa um evento que tem apenas tempo e base, como o caso do avisa */
+struct ev_t_dois{
+    int tempo;
+    int id_base;
+};
+
+/* representa um evento que usa apenas tempo e missao */
+struct ev_t_tres{
+    int tempo;
+    int id_missao;
+}
+
+/* representa um heroi H chegando em uma base B no instante T, para depois esperar para entrar na fila ou desistir */
+void chega(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa a entrada do heroi na fila de espera */
+void espera(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa a desistencia de um heroi em esperar para entrar em uma base, e em seguida esse heroi viaja para uma base aleatoria D */
+void desiste(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa que o porteiro da dada base eh avisado e verifica a fila de espera */
+void avisa(struct mundo_t *m, struct ev_t_dois *ev);
+
+/* representa a entrada do heroi na determinada base, agendando o tempo que vai permanecer la e quando vai sair */
+void entra(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa a saida do heroi da base em que estava, escolhendo uma outra base aonde viajar e avisando o porteiro de que ha uma nova vaga disponivel */
+void sai(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa a viagem do heroi para uma base, agendando e avisando o ponteiro */
+void viaja(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa a morte de um heroi, liberando uma nova vaga e avisando o porteiro */
+void morre(struct mundo_t *m, struct ev_t_um *ev);
+
+/* representa o disparo de uma missao nova no instante T*/
+void evento_missao(struct mundo_t *m, struct ev_t_tres *ev);
+
+/* representa o fim da simulacao */
+void fim(struct mundo_t *m);
+
+/* cria os primeiros eventos e agenda o fim do mundo */
+void inicio(struct mundo_t *m);
