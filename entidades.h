856a4a36c618d4390043bcdf66694cc6185db6fc
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-28 02:50:57 -0300

    definicoes

--- entidades.h
+++ entidades.h
@@ -3,6 +3,14 @@
 #include "fprio.h"
 #include "conjunto.h"
 #include "fila.h"
+#define T_INICIO 0
+#define T_FIM_DO_MUNDO 525600
+#define N_TAMANHO_MUNDO 20000
+#define N_HABILIDADES 10
+#define N_HEROIS 5*N_HABILIDADES
+#define N_BASES N_HEROIS/5
+#define N_MISSOES T_FIM_DO_MUNDO/100
+#define N_COMPOSTOS_V N_HABILIDADES*3
 
 struct heroi_t();
 
