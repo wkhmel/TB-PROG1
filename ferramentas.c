1898ae63548995ed2f47bf8c1c810599f34c080b
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-29 04:57:40 -0300

    ferramentas.c

--- ferramentas.c
+++ ferramentas.c
@@ -7,6 +7,11 @@ struct coord_t{
     int y;
 }
 
+struct dist_base {
+    int id;
+    int distancia;
+};
+
 struct coord_t cria_coord(int x, int y){
     struct coord c;
     c.x = int x;
@@ -23,11 +28,6 @@ long aleat(long min, long max)
 void atualiza_tempo(struct mundo_t *w, struct evento_t *ev){
     w->tempo = ev->tempo;
 }
-
-struct dist_base {
-    int id;
-    int distancia;
-};
 
 int dist_coord(struct coord_t c1, struct coord_t c2){
     return sqrt(pow(c2.x - c1.x) + pow(c2.y - c1.y));     
