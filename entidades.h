4ca6556e2a96d1dcc7f7124ff1f0fde7fba58969
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 02:54:44 -0300

    mundo

--- entidades.h
+++ entidades.h
@@ -38,6 +38,6 @@ struct missao_t *destroi_missao(struct missao_t *m);
 
 struct mundo_t *cria_mundo();
 
-struct missao_t *destroi_mundo(struct mundo_t *w);
+struct mundo_t *destroi_mundo(struct mundo_t *w);
 
 #endif
