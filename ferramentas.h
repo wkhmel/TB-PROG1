6a02f888565c218f303a6c27bcac555ee824a98f
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 20:42:57 -0300

    fazendo comentarios

--- ferramentas.h
+++ ferramentas.h
@@ -3,20 +3,27 @@
 #include <math.h>
 #include <stdlib.h>
 
+/* estrutura de coordenadas x e y */
 struct coord_t{
     int x;
     int y;
 };
 
+/* estrutura que armazena o id de uma base e a distancia dela ate algum lugar. */
+/* eh util para ver qual eh a base de menor distancia ate uma missao. */
 struct dist_base{
     int id;
     int distancia;
 };
 
+/* funcao de aleatorizacao */
 long aleat(long min, long max);
 
+/* calcula a distancia entre coordenadas */
 int dist_coord(struct coord_t c1, struct coord_t c2);
 
+/* ordena as distancias de uma estrutura contendo id e distancia e */
+/* recebe um parametro com a quantidade de elementos */
 void ordena_dist(struct dist_base v[], int qtd);
 
 #endif
