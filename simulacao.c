10a3c485b5f8026ba1876bca323dc56fba397884
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 00:38:25 -0300

    chega

---
+++ simulacao.c
@@ -0,0 +1,63 @@
+#include "simulacao.h"
+#include "eventos.h"
+#include "fprio.h"
+#include "entidades.h"
+
+void simular_eventos(struct mundo_t *w){
+    struct evento_t *ev;
+    int tipo_evento, tempo, id_h;
+    int fim = 0;
+    if (!verifica_mundo(w))
+        return;
+
+    while ((ev = fprio_retira(w->lef, &tipo_evento, &tempo))){
+        if (tipo_evento < AVISA){
+            id_h = ev->id_h;
+            
+            if ((w->vet_h[h_id])->morto){
+                free(ev);
+            }
+        }
+
+        w->tempo = tempo;
+        m->total_eventos++;
+
+    /* escolhe o evento */
+        switch (tipo_evento) {
+            case CHEGA:
+                 chega(w, ev);
+                break;
+            case ESPERA:
+                espera(w, ev);
+                break;
+            case DESISTE:
+                desiste(w, ev);
+                break;
+            case MORRE:
+                morre(w, ev);
+                break;
+            case ENTRA:
+                entra(w, ev);
+                break;
+            case SAI:   
+                sai(w, ev);
+                break;
+            case VIAJA:
+                viaja(w, ev);
+                break;
+            case AVISA:
+                avisa(w, ev);
+                break;
+            case MISSAO:
+                missao(w, ev);
+                break;
+            case FIM:
+                fim(w, ev);
+                return;
+                fim = 1;
+        }
+
+        free(ev);
+        ev = NULL;
+    }
+}
