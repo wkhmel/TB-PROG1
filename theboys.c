478224443196d15db08c5c7382836199198f4837
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 01:17:51 -0300

    e o que sobra

--- theboys.c
+++ theboys.c
@@ -1,9 +1,10 @@
 // programa principal do projeto "The Boys - 2024/2"
-// Autor: xxxxx, GRR xxxxxx
-
-// seus #includes vão aqui
-
-// seus #defines vão aqui
+// Autor: Melissa Goulart Kemp, GRR 20255413
+#include <stdlib.h>
+#include "entidades.h"
+#include "simulacao.h"
+#include "ferramentas.h"
+#include "eventos.h"
 
 // minimize o uso de variáveis globais
 
@@ -11,11 +12,24 @@
 int main ()
 {
   // iniciar o mundo
-
+  struct mundo_t *w;
+  struct evento_t *ev;
+  
   // executar o laço de simulação
-
+  w = cria_mundo();
+  inicia_eventos(w);
+  while (w->tempo < T_FIM_DO_MUNDO){
+    /*Retira o primeiro evento da lista de eventos futuros*/
+    ev = retira_lef(retorna_evento(mundo));
+    atualiza_relogio(w, ev);
+    /*Chama a função que trata o evento*/
+    simula_eventos(w);
+    /*Liberta a memoria do evento*/
+    destroi_evento(ev);
+  }
+  
   // destruir o mundo
-
-  return (0) ;
+  destroi_mundo(w);
+  return 0;
 }
 
