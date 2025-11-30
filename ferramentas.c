e7265004f762ae748cafbec4fc2620272db6b8c7
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 20:40:31 -0300

    comentei o ferramentas.c

--- ferramentas.c
+++ ferramentas.c
@@ -2,22 +2,25 @@
 #include <math.h>
 #include "ferramentas.h"
 
-// (max - min + 1) gera um número entre 0 e (max - min)
-// Somando min, temos o deslocamento do resultado para o intervalo [min, max]
+/* (max - min + 1) gera um valor entre 0 e (max - min) */
 long aleat(long min, long max){
   return rand() % (max - min + 1) + min;
 }
 
+/* calcula a distancia de dos locais usando a formula da distancia entre coordenadas */
 int dist_coord(struct coord_t c1, struct coord_t c2){
     return sqrt(pow(c2.x - c1.x, 2) + pow(c2.y - c1.y, 2));     
 }
 
+/* ordena as distancias usando o algoritmo de selectionsort. */
+/* percorre todo o vetor nao ordenado em cada iteracao para */
+/* buscar o menor elemento. */
 void ordena_dist(struct dist_base v[], int qtd){
     int menor;
     for (int x = 0; x < qtd - 1; x++){
         menor = x;
         for (int y = x + 1; y < qtd; y++){
-            if (v[y].distancia < v[menor].distancia)
+            if (v[y].distancia < v[menor].distancia) /* se for menor que a menor, temos uma nova menor */
                 menor = y;
         }
             if (menor != x){
