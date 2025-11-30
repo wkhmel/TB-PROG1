a3640a3d9d119a7456a4e1d1c9c3cd8a387af8e2
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 02:19:20 -0300

    ta

--- ferramentas.c
+++ ferramentas.c
@@ -25,10 +25,6 @@ long aleat(long min, long max)
   return rand() % (max - min + 1) + min;
 }
 
-void atualiza_tempo(struct mundo_t *w, struct evento_t *ev){
-    w->tempo = ev->tempo;
-}
-
 int dist_coord(struct coord_t c1, struct coord_t c2){
     return sqrt(pow(c2.x - c1.x) + pow(c2.y - c1.y));     
 }
