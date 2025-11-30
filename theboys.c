8df375f4d9ae03f84623474ea6d34e35d7565775
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 17:45:53 -0300

    minha main

--- theboys.c
+++ theboys.c
@@ -9,15 +9,18 @@
 int main (){
   // iniciar o mundo
   struct mundo_t *w;
-  srand(0);
-  // executar o laço de simulação
+  srand(0); 
+
   w = cria_mundo();
-  while (tempo_mundo(w)){
-    inicia_eventos(w);
-    simula_eventos(w);
-  }
-  // destruir o mundo
+
+  // agenda os eventos iniciais uma unica vez
+  inicia_eventos(w);
+
+  // roda a simulacao do mundo
+  simula_eventos(w);
+
+  // destroi tudo no fim da simulacao
   destroi_mundo(w);
+
   return 0;
 }
-
