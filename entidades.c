c9d8be4551d7ef4f98183c0aca686b4285141bdd
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 14:19:17 -0300

    sss

--- entidades.c
+++ entidades.c
@@ -1,178 +1,390 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "ferramentas.h"
 #include "entidades.h"
+#include "eventos.h"
 #include "fprio.h"
 #include "conjunto.h"
 #include "fila.h"
+#include "ferramentas.h"
 
-/* cria um heroi. retorna o ponteiro para o heroi ou NULL em caso de erro. */
-struct heroi_t *cria_heroi(int id){
-    struct heroi_t *h;
-    h = malloc(sizeof(struct heroi_t));
-    if (!h)
-        return NULL;
-    h->id_h = id;
-    h->skills = cjto_aleat(aleat(1, 3), N_HABILIDADES);
-    h->paciencia = aleat(0, 100);
-    h->speed = aleat(50, 5000);
-    h->exp = 0;
-    h->id_b = -1;
-    h->morto = false;
-
-    return h;
+/* cria um novo evento, retornando um ponteiro para ele ou NULL em caso de erro */
+int adiciona_evento(struct mundo_t *w, int tempo, int tipo, int info1, int info2){
+    struct evento_t *ev = malloc(sizeof(struct evento_t));
+    if (!ev)
+        return -1;
+    ev->tempo = tempo;
+    ev->tipo = tipo;
+    ev->info1 = info1;
+    ev->info2 = info2;
+    int teste = fprio_insere(w->lef, ev, tipo, tempo);
+    if (teste < 0)
+        return -1;
+    return 1;
 }
 
-/* funcao que destroi o heroi e todas as suas estruturas e devolve o id do heroi em um ponteiro. */
-/* retorna NULL. */
-struct heroi_t *destroi_heroi(struct heroi_t *h){
-    if (!h)
-        return NULL;
-    if (h->skills)
-        cjto_destroi(h->skills);
-    free(h);
-    return NULL;
+int verifica_mundo(struct mundo_t *w){
+    if (!w || !w->vet_h || !w->vet_b || !w->vet_m || !w->lef)
+        return 0;
+    return 1;
 }
 
-struct base_t *cria_base(int id){
-    struct base_t *b;
-    b = malloc(sizeof(struct base_t));
-    if (!b)
-        return NULL;
-    b->id_b = id;
-    b->limite = aleat(3, 10);
-    b->presentes = cjto_cria((b->limite)); /* cria conjunto vazio */
-    b->espera = fila_cria(); /* cria fila de espera vazia */
-    b->missoes = 0;
-    b->max_fila = 0;
-
-    return b;
-}
-
-struct base_t *destroi_base(struct base_t *b){
-    if (!b)
-        return NULL;
-    if (b->presentes)
-        cjto_destroi(b->presentes);
-    if (b->espera)
-        fila_destroi(b->espera);
-    free(b);
-    return NULL;
-}
-
-struct missao_t *cria_missao(int id){
-    struct missao_t *m;
-    m = malloc(sizeof(struct missao_t));
-    if (!m)
-        return NULL;
-    m->id_m = id;
-    m->skills = cjto_aleat(aleat(6,10), N_HABILIDADES);
-    m->tentativas = 0;
-    m->realizou = false;
-    m->local.x = aleat(0, N_TAMANHO_MUNDO - 1);
-    m->local.y = aleat(0, N_TAMANHO_MUNDO - 1);
-    return m;
-}
-
-struct missao_t *destroi_missao(struct missao_t *m){
-    if (!m)
-        return NULL;
-    if (m->skills)
-        cjto_destroi(m->skills);
-    free(m);
-    return NULL;
-}
-
-/* inicializacao */
-struct mundo_t *cria_mundo(){
-    struct mundo_t *w;
-    w = malloc(sizeof(struct mundo_t));
-    if (!w)
-        return NULL;
-    w->qtd_h = N_HEROIS;
-    w->qtd_b = N_BASES;
-    w->qtd_m = N_MISSOES;
-
-    if (!w->vet_h || !w->vet_b || !w->vet_m){
-        destroi_mundo(w);
-        return NULL;
-    }   
-    
-    w->vet_h = malloc(w->qtd_h*(sizeof(struct heroi_t));
-    w->vet_b = malloc(w->qtd_b*(sizeof(struct base_t));
-    w->vet_m = malloc(w->qtd_m*(sizeof(struct missao_t));
-
-    /* preenche vetor de struct heroi_t */
+int inicia_eventos(struct mundo_t *w){
+    int base, tempo; 
+    if (!verifica_mundo(w))
+        return 0;
+/* chegada dos herois */
     for (int i = 0; i < w->qtd_h; i++){
-        struct heroi_t *aux = cria_heroi(i);
-        if (!aux)
-            return NULL;
-        }
-        w->vet_h[i] = *aux;
-        free(aux);
+        base = aleat(0, w->qtd_b - 1);
+        tempo = aleat(0, 4320);
+        if (!(adiciona_evento(w, tempo, CHEGA, (w->vet_h[i])->id_h, base)))
+            return 0;
     }
 
-    for (int i = 0; i < w->qtd_b; i++){
-        struct base_t *aux = cria_base(i);
-        if (!aux)
-            return NULL;
-        }
-        w->vet_b[i] = *aux;
-        free(aux);
-    }
-
+/* agendamento das missoes */
     for (int i = 0; i < w->qtd_m; i++){
-        struct missao_t *aux = cria_missao(i);
-        if (!aux)
-            return NULL;
-        }
-        w->vet_m[i] = *aux;
-        free(aux);
+        tempo = aleat(0, T_FIM_DO_MUNDO);
+        if (!(adiciona_evento(w, tempo, MISSAO, (w->vet_m[i])->id_m, -1)))
+            return 0;
     }
 
-    w->qtd_hab = N_HABILIDADES;
-    w->qtd_v = N_COMPOSTOS_V;
-    (w->tam_m).x = N_TAMANHO_MUNDO;
-    (w->tam_m).y = N_TAMANHO_MUNDO;
-    w->tempo = T_INICIO;
-    w->lef = fprio_cria();
-    w->total_eventos = 0;
-    w->missoes_cumpridas = 0;
-    w->mortes = 0;
-    return w;
+/* agendamento do fim do mundo */
+    if (!adiciona_evento(w, T_FIM_DO_MUNDO, FIM, -1, -1));
+        return 0;
+    return 1;
 }
 
-struct mundo_t *destroi_mundo(struct mundo_t *w){
-    if (!w)
+/* representa um heroi H chegando em uma base B no instante T, para depois esperar para entrar na fila ou desistir */
+void chega(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[ev->info2];
+    if (!verifica_mundo(w))
+        return;
+    h->id_b = b->id_b; /* atualiza a base do heroi */
+    int tempo = ev->tempo;
+    int espera, tipo_ev;
+    char *texto;
+    if (cjto_card(b->presentes) < (b->limite) && fila_tamanho(b->espera) == 0)
+        espera = 1; /* simboliza o valor logico verdadeiro, e nao a cardinalidade da fila de espera */
+    else espera = (h->paciencia) > (10*fila_tamanho(b->espera));
+    if (espera){
+        tipo_ev = ESPERA;
+        texto = "ESPERA";
+    }
+    else {
+        tipo_ev = DESISTE;
+        texto = "DESISTE";
+    }
+    if (!adiciona_evento(w, tempo, tipo_ev, h->id_h, b->id_b))
+        return;
+    printf("%6d: CHEGA HEROI %2d BASE %d (%2d/%2d) %s\n", tempo, h->id_h, b->id_b, cjto_card(b->presentes), b->limite, texto);    
+}
+
+/* representa a entrada do heroi na fila de espera */
+void espera(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    if (!(fila_insere(b->espera, h->id_h)))
+        return;
+    if (!adiciona_evento(w, tempo, AVISA, h->id_h, b->id_b))
+        return;
+    int atual = fila_tamanho(b->espera);
+    if (atual > b->fila_max)
+        b->fila_max = atual;
+    printf("%6d: ESPERA HEROI %2d BASE %d (%2d)\n", tempo, h->id_h, b->id_b, fila_tamanho(b->espera));    
+}
+
+/* representa a desistencia de um heroi em esperar para entrar em uma base, e em seguida esse heroi viaja para uma base aleatoria D */
+void desiste(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    int base_destino = aleat(0, w->qtd_b - 1);
+    if (!adiciona_evento(w, tempo, VIAJA, h->id_h, b->id_b))
+        return;
+    printf("%6d: DESIST HEROI %2d BASE %d", tempo, h->id_h, b->id_b);
+}
+
+/* representa que o porteiro da dada base eh avisado de que liberou uma vaga. */
+/* verifica a fila de espera. */
+void avisa(struct mundo_t *w, struct evento_t *ev){
+    struct base_t *b = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    printf("%6d: AVISA PORTEIRO BASE %d (%2d/%2d) FILA [ ", tempo, b->id_b, cjto_card(b->presentes), b->limite);
+    fila_imprime(b->espera);
+    printf(" ]\n");
+    while (cjto_card(b->presentes) < b->limite && fila_tamanho(b->espera) > 0){
+        int heroi;
+        fila_retira(b->espera, &heroi);
+        int teste = cjto_insere(b->presentes, heroi);
+        if (teste < 0)
+            return;
+        if (!adiciona_evento(w, tempo, ENTRA, heroi, b->id_b))
+            return;
+        printf("%6d: AVISA PORTEIRO BASE %d ADMITE %2d", tempo, b->id_b, heroi);
+    }
+
+}
+
+/* representa a entrada do heroi na determinada base, agendando o tempo que vai permanecer la e quando vai sair */
+void entra(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    int tpb = 15 + (h->paciencia)*aleat(1, 20);
+    if (!adiciona_evento(w, tempo + tbp, SAI, h->id_h, b->id_b))
+        return;    
+    printf("%6d: ENTRA HEROI %2d BASE %d (%2d/%2d) SAI %d\n", tempo, h->id_h, b->id_b, cjto_card(b->presentes), b->limite, tempo + tpb);
+}
+
+/* representa a saida do heroi da base em que estava, escolhendo uma outra base aonde viajar e avisando o porteiro de que ha uma nova vaga disponivel */
+void sai(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    cjto_retira(b->presentes, h->id_h);
+    int base_destino = aleat(0, w->qtd_b - 1);
+    if (!adiciona_evento(w, tempo, VIAJA, h->id_h, base_destino) || !adiciona_evento(w, tempo, AVISA, -1, base_destino))
+        return;
+    printf("%6d: SAI HEROI %2d BASE %d (%2d/%2d)\n", tempo, h->id_h, b->id_b, cjto_card(b->presentes), b->limite);
+}
+
+/* representa a viagem do heroi para uma base, agendando e avisando o ponteiro */
+void viaja(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[h->id_b];
+    struct base_t *d = w->vet_b[ev->info2];
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w))
+        return;
+    distancia = dist_coord(b->local, d->local);
+    int duracao = distancia/(h->speed);
+    if (!adiciona_evento(w, tempo + duracao, CHEGA, h->id_h, d->id_b)
+        return;
+    printf("%6d: VIAJA HEROI %2d BASE %d BASE %d DIST %d VEL %d CHEGA %d", tempo, h->id_h, b->id_b, d->id_b, distancia, h->speed, tempo + duracao); 
+}
+
+/* representa a morte de um heroi, liberando uma nova vaga e avisando o porteiro */
+void morre(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h = w->vet_h[ev->info1];
+    struct base_t *b = w->vet_b[h->id_b];
+    struct missao_t *m = w->vet_m[ev->info2].
+    int tempo = ev->tempo;
+    if (!verifica_mundo(w) || !b->presentes || h->morreu)
+        return;
+    printf("%6d: MORRE HEROI %2d MISSAO %d", tempo, h->id_h, m->id_m);
+    if (cjto_retira(b->presentes, h->id_h) < 0)
+        return;
+    h->morreu = true;
+    w->mortes++;
+    if (!adiciona_evento(w, tempo, AVISA, -1, b->id_b))
+        return;    
+}
+
+/* une todas as habilidades dos herois de uma mesma base */
+/* verifica se o heroi esta naquela base e, se sim, adiciona suas habilidades ao conjunto */
+struct cjto_t *skills_b(struct mundo_t *w, int id_b){
+    if (!verifica_mundo(w))
+        return;
+    struct cjto_t *uniao = cjto_cria(N_HABILIDADES); /* cria um conjunto vazio com capacidade para ate N_HABILIDADES */
+    if (!uniao)
         return NULL;
-    if (w->vet_h){
-        
-        for (int i = 0; i < w->qtd_h; i++){
-            destroi_heroi(w->vet_h[i]);
-        }
-        free(w->vet_h);
+    
+    for (int i = 0; i < w->qtd_h; i++){
+        if (cjto_pertence((w->vet_b[id_b])->presentes, i))
+            uniao = cjto_uniao(uniao, (w->vet_h[i])->skills);
     }
-
-    if (w->vet_b){
-        
-        for (int i = 0; i < w->qtd_b; i++){
-            destroi_base(w->vet_b[i]);
-        }
-        free(w->vet_b);
-    }
-
-    if (w->vet_m){
-        
-        for (int i = 0; i < w->qtd_m; i++){
-            destroi_missao(w->vet_m[i]);
-        }
-        free(w->vet_m);
-    }
-
-    if (w->lef)
-        fprio_destroi(w->lef);
-
-    free(w);
-    return NULL;
+    return uniao;
 }
+
+int maior_xp(struct mundo_t *w, int id_b){
+    if (!verifica_mundo(w))
+        return -1;
+    int maior = -1;
+    int maior_id = -1;
+    for (int i = 0; i < w->qtd_h; i++){
+        if (cjto_pertence(w->vet_b[id_b]->presentes), i){
+            if ((w->vet_h[i])->exp > maior){
+                maior = (w->vet_h[i])->exp;
+                maior_id = (w->vet_h[i])->id_h;
+            }
+        }
+
+    }
+    return maior_id;
+
+}
+
+/* representa o disparo de uma missao nova no instante T*/
+void evento_missao(struct mundo_t *w, struct evento_t *ev){
+    struct missao_t *m = w->vet_m[ev->info1];
+    struct base_t *b;
+    struct dist_base dist[N_BASES]; /* struct de distancia de cada base ate a missao */
+    int tempo = ev->tempo;    
+    if (!verifica_mundo(w) || m->realizou)
+        return;
+    m->tentativas++;
+    /* verifica a distancia de cada base em relacao ao local da missao */
+    for (int i = 0; i < N_BASES; i++){
+        dist[i].id = i;
+        dist[i].distancia = dist_coord(m->local, w->vet_b[i]->local);
+    }
+    ordena_dist(dist, N_BASES);
+    int base_missao = -1;
+
+    printf("%6d: MISSAO %d TENT %d HAB REQ: ", tempo, m->id_m, m->tentativas);
+    cjto_imprime(m->skills);
+    printf("\n");
+ 
+    for (int i = 0; i < w->qtd_b; i++){
+        struct cjto_t *uni = skills_b(w, dist[i].id);
+        if (cjto_contem(uni, m->skills)){
+            base_missao = dist[i].id;
+        }
+        cjto_destroi(uni);
+    }
+
+    if (base_missao >= 0){
+        m->realizou = true;
+        (w->vet_b[base_missao])->missoes++;
+        w->missoes_cumpridas++;
+        printf("%6d: MISSAO %d CUMPRIDA BASE %d HABS: [ ", tempo, m->id_m, base_missao);
+        cjto_imprime((w->vet_b[base_missao])->presentes);
+        printf("\n");
+        /* aumentar xp dos herois */
+        for (j = 0; j < w->qtd_h; j++){
+            if (cjto_pertence((w->vet_b[base_missao])->presentes, j))
+                (w->vet_h[j])->exp++;
+        }
+        return;
+    }
+    if ((w->qtd_v > 0) && (tempo%2500 == 0)){
+        w->qtd_v--;
+        m->realizou = true;
+        adiciona_evento(w, tempo, MORRE, maior_xp(w, 0), 0)
+        return;
+    }
+
+    printf("%6d: MISSAO %d IMPOSSIVEL", tempo, m->id_m);
+    /* coloca na agenda de novo */
+    if (!adiciona_evento(w, tempo + 24*60, EV_MISSAO, m->id_m, -1));
+        return;
+}
+
+int tempo_mundo(struct mundo_t *w){
+    return w->tempo;
+}
+
+/* representa o fim da simulacao */
+void fim(struct mundo_t *w, struct evento_t *ev){
+    struct heroi_t *h;
+    struct base_t *b;
+    int tempo = ev->tempo;
+
+    if (!verifica_mundo)
+        return;
+
+    printf("%6d: FIM", T_FIM_DO_MUNDO);
+    for (int i = 0; i < N_HEROIS; i++){
+        if ((w->vet_h[i])->morreu)
+            printf("HEROI %2d MORTO ", i);
+        else
+            printf("HEROI %2d VIVO ", i);
+        printf("PAC %3d VEL %4d EXP %4d HABS [ ", (w->vet_h[i])->paciencia, (w->vet_h[i])->speed, (w->vet_h[i])->exp);
+        cjto_imprime((w->vet_h[i])->skills);
+        printf(" ]\n");
+    }
+
+    for (int i = 0; i < N_BASES; i++){
+        struct base_t *base = w->vet_b[i];
+        printf("BASE %2d LOT %2d FILA MAX %2d MISSOES %d", i, base->limite, base->fila_max, base->missoes);
+    }
+    float taxa_missoes = (w->missoes_cumpridas)/(N_MISSOES);
+    printf("EVENTOS TRATADOS: %d", w->total_eventos);
+    printf("MISSOES CUMPRIDAS: %d/%d (%.1f%%)", w->missoes_cumpridas, N_MISSOES, taxa_missoes);
+    int max_tentativas = 0;
+    for (a = 0; a < w->qtd_m; a++){
+        if (((w->vet_m[a])->tentativas) > max_tentativas)
+            max_tentativas = (w->vet_m[a])->tentativas;
+    }
+    int min_tentativas = max_tentativas;
+    for (a = 0; a < w->qtd_m; a++){
+        if (min_tentativas > (w->vet_m[a])->tentativas)
+            min_tentativas = (w->vet_m[a])->tentativas;
+    }     
+    int soma = 0;
+    for (a = 0; a < w->qtd_m; a++){
+        soma = soma + (w->vet_m[a])->tentativas;
+    }            
+    float media = (float)soma/w->qtd_m;
+    float mortalidade = (float)w->mortes/w->qtd_h;
+    printf("TENTATIVAS/MISSAO: MIN %d, MAX %d, MEDIA %.1f", min_tentativas, max_tentativas, media);
+    printf("TAXA MORTALIDADE: %.1f%%", mortalidade);
+}
+
+void simula_eventos(struct mundo_t *w){
+    struct evento_t *ev;
+    int tipo_evento, tempo, id_h;
+    int fim = 0;
+    if (!verifica_mundo(w))
+        return;
+    while (!fim && fprio_tamanho(lef) > 0){
+        ev = fprio_retira(w->lef, &tipo_evento, &tempo);
+        if (!ev)
+            break;
+        if ((tipo_evento != MISSAO) && (tipo_evento != AVISA) && (tipo_evento != FIM)){
+            struct heroi_t *h = w->vet_h[ev->info1];
+            if ((h->morto))
+                free(ev);
+                break;
+        }
+        tempo_mundo(w) = tempo;
+        w->total_eventos++;
+                
+        switch(tipo_evento){        
+            case CHEGA:
+                chega(w, ev);
+                break;
+            case ESPERA:
+                espera(w, ev);
+                break;
+            case DESISTE:
+                desiste(w, ev);
+                break;
+            case MORRE:
+                morre(w, ev);
+                break;
+            case ENTRA:
+                entra(w, ev);
+                break;
+            case SAI:
+                sai(w, ev);
+                break;
+            case VIAJA:
+                viaja(w, ev);
+                break;
+            case AVISA:
+                avisa(w, ev);
+                break;
+            case MISSAO:
+                evento_missao(w, ev);
+                break;
+            case FIM:
+                fim(w, ev);
+                fim = 1;
+        }
+
+    }
+    free(ev);
+}
+
