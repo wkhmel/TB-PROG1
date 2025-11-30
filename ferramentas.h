60d7ce8363b68bc1e77f03ce3ca2a59fdf5eedaf
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2025-11-30 02:19:31 -0300

    tokyo

--- ferramentas.h
+++ ferramentas.h
@@ -13,8 +13,6 @@ struct coord_t cria_coord(int x, int y);
 
 long aleat(long min, long max);
 
-void atualiza_tempo(struct mundo_t *w, struct evento_t *ev);
-
 int dist_coord(struct coord_t c1, struct coord_t c2);
 
 void ordena_dist(int vet_dist[], int vet_id[], int qtd);
